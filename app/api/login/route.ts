40e7f96bf5e64f92c2b9216d990b0be7cefc4008
Mohamed fetoh <mi73@gatech.edu>
2025-12-21 13:48:45 +0000

    update package

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import { NextResponse } from "next/server"
 import { pool } from "@/lib/db"
--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { pool } from "@/lib/db"
 
 export async function POST(req: Request) {
